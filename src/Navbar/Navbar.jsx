870b2b1306fbc0d90cef798baa61215254119aa4
Aman Thukral <thukral618@gmail.com>
2022-01-09 03:01:51 +0530

    Adding basic layout of navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
+import BlogViewIcon from '../assets/Images/BlogView.png'
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='bg-purple-500'>
+            <h4><img src={BlogViewIcon} alt="" />BlogView</h4>
+            <span>⭐ Star on Github</span>
         </div>
     )
 }
