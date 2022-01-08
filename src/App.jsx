ca4d4a23934ac318f0ffdc0d470490bee5fecbd1
Aman Thukral <thukral618@gmail.com>
2022-01-08 20:41:25 +0530

    Setting up the folder and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import BlogConfig from "./Mainbody/BlogConfig"
+import Navbar from "./Navbar/Navbar"
+
 function App() {
 
   return (
     <div className="text-white bg-black h-screen">
-     Hello
+     <Navbar />
+     <BlogConfig />
     </div>
   )
 }
---
+++ src/Mainbody/BlogConfig.jsx
@@ -0,0 +1,9 @@
+const BlogConfig = () => {
+    return (
+        <div>
+            BlogConfig
+        </div>
+    )
+}
+
+export default BlogConfig
