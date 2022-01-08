d7ff308f095518077fce8eba3dbc693917732669
Aman Thukral <thukral618@gmail.com>
2022-01-09 03:20:03 +0530

    Adding styling for navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import BlogViewIcon from '../assets/Images/BlogView.png'
 const Navbar = () => {
     return (
-        <div className='bg-purple-500'>
-            <h4><img src={BlogViewIcon} alt="" />BlogView</h4>
-            <span>⭐ Star on Github</span>
+        <div className='bg-purple-500 flex justify-between p-1'>
+           <h4 className='flex my-auto ml-4 mr-auto font-bold text-2xl'>
+                <img src={BlogViewIcon} alt="Icon" className='h-8 w-8 m-1 mr-2' />BlogView</h4>
+                <a href="https://github.com/Amanthukral12/BlogView" className='p-1 my-1 mx-2 transform hover:scale-x-105'>
+                    <span className='bg-gray-900 text-sm font-semibold p-1 my-1 ml-auto rounded'>
+                        ⭐ Star on Github</span>
+                        </a>
         </div>
     )
 }
