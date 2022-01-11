a8e786dd73af2ab048ba3882ca7cca30b23e15d9
Aman Thukral <thukral618@gmail.com>
2022-01-11 17:59:54 +0530

    Adding styling for blogimage component

--- src/Mainbody/BlogImage.jsx
+++ src/Mainbody/BlogImage.jsx
@@ -1,11 +1,17 @@
 const BlogImage = ({title, bgColor, author, icon}) => {
     return (
-        <div>
-            <div style={{backgroundColor: bgColor}}>
-                <i className={`devicon-${icon.value}-plain text-8xl`}></i>
-                <div>{title}</div>
-                <div>{author}</div>
+        <div style={{backgroundColor: bgColor}} 
+            className="flex flex-row w-full h-80 py-12 px-16 text-gray-600 ">
+                <div  className="w-full bg-white flex p-12 pr-8 rounded-lg">
+                <i className={`devicon-${icon.value}-plain text-8xl m-4 mr-5`}></i>
+                    <div className="flex flex-col">
+                        <h1 className="text-4xl font-bold mb-4">{title}</h1>
+                        <hr className="border-2 bg-gray-800 w-1/2 mb-2" />
+                        <h3 className="text-3xl font-bold">{author}</h3>
+                    </div>
+                    
                 </div>
+            
         </div>
     )
 }
