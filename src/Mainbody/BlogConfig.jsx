5a40fcb72a1499af721ccb0bea5029f111901285
Aman Thukral <thukral618@gmail.com>
2022-11-09 17:31:24 +0530

    Adding functionality to change the font

--- src/Mainbody/BlogConfig.jsx
+++ src/Mainbody/BlogConfig.jsx
@@ -1,110 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import BlogImage from './BlogImage';
-import Select from 'react-select';
-import ComponentToImg from './ComponentToImg';
+import React, { useState, useEffect } from "react";
+import BlogImage from "./BlogImage";
+import Select from "react-select";
+import ComponentToImg from "./ComponentToImg";
 const BlogConfig = () => {
-    const [title, setTitle] = useState("How I built my first project with react");
-    const [author, setAuthor] = useState("Aman Thukral");
-    const [bgColor, setBgColor] = useState("#8B5CF6")
-    const [icons, setIcons] = useState({});
-    const [icon, setIcon] = useState({'label': 'react', 'value': 'react'})
-    const [downloadAs, setDownloadAs] = useState("PNG")
+  const [title, setTitle] = useState("How I built my first project with react");
+  const [author, setAuthor] = useState("Aman Thukral");
+  const [bgColor, setBgColor] = useState("#8B5CF6");
+  const [icons, setIcons] = useState({});
+  const [icon, setIcon] = useState({ label: "react", value: "react" });
+  const [downloadAs, setDownloadAs] = useState("PNG");
+  const [fontStyle, setFontStyle] = useState("font-Anek");
 
-    const reset = () => {
-        setTitle('How I built my first project with react')
-        setAuthor('Aman Thukral')
-        setBgColor('#8B5CF6')
-        setIcon({'label': 'react', 'value': 'react'})
-        setDownloadAs('PNG')
-    }
+  const reset = () => {
+    setTitle("How I built my first project with react");
+    setAuthor("Aman Thukral");
+    setBgColor("#8B5CF6");
+    setIcon({ label: "react", value: "react" });
+    setDownloadAs("PNG");
+  };
 
-    const getIcons = async () => {
-        const response = await fetch('https://raw.githubusercontent.com/devicons/devicon/master/devicon.json');
-        const data = await response.json();
-        setIcons(data.map((item) => ({'value': item.name, 'label': item.name})));
-    }
+  const getIcons = async () => {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/devicons/devicon/master/devicon.json"
+    );
+    const data = await response.json();
+    setIcons(data.map((item) => ({ value: item.name, label: item.name })));
+  };
 
-    useEffect(() => {
-        getIcons();
-    },[])
+  useEffect(() => {
+    getIcons();
+  }, []);
 
-    return (
-        <div className="flex xl:flex-row flex-col text-black bg-gray-100 h-screen">
-            <div className="w-full xl:w-2/5 bg-white">
-                <h6 className='mt-8 ml-8 font-medium text-gray-500 mb-8'>💻 Creating cover images for your<br /> 
-                <span className='ml-8'>blogs is now super easy</span></h6>
-                <div className='border-2 border-gray-200 w-11/12 ml-auto mr-auto rounded mt-2 mb-1'>
-                    <input type="text"
-                        placeholder='Enter title here'  
-                        onChange={(e) => {
-                            setTitle(e.target.value)
-                        }}
-                        className='w-full focus:outline-none focus:ring focus:border-blue-300
-                        leading-5 p-2 rounded text-center '
-                        />
-                </div>
-                <div className='border-2 border-gray-200 w-11/12 ml-auto mr-auto rounded mt-1'>
-                    <input type="text"
-                    placeholder='Enter Author here'
-                    onChange={(e) => {
-                        setAuthor(e.target.value)
-                    }}
-                    className='w-full focus:outline-none focus:ring focus:border-blue-300
-                        leading-5 p-2 rounded text-center '
-                    />
-                </div>
-                <div className='w-11/12 border-2 mt-3 rounded text-center m-auto'>
-                    <h6 className='font-semibold text-gray-600 mb-4'>Background</h6>
-                    <div className='flex flex-row mb-2'>
-                    <div className='mr-auto ml-auto w-24 justify-center text-center '>
-                        <span>Color
-                    <input type="color"
-                    value={bgColor}
-                    onChange={(e) => {
-                        setBgColor(e.target.value)
-                    }}
-                    className='w-24 h-10 pr-3 pl-3 p-1 bg-white border-2 flex-col rounded' />
-                    </span>
-                    </div>
-                    </div> 
-                </div>
-
-                <div className='text-center mt-2 w-4/5 m-auto'>
-                <h6 className='font-semibold text-gray-600 mb-2'>Dev Icon</h6>
-                <Select
-                    value={icon}
-                    options={icons}
-                    onChange={(e) => {
-                        setIcon(e);
-                    }}
-                    className='uppercase'
-                />
-                </div>
-                <div className='text-center mt-2 w-4/5 m-auto'>
-                    <h6 className='font-semibold text-gray-600 mb-2'>Download As</h6>
-                    <select
-                        onChange={(e) => setDownloadAs(e.target.value)}
-                        value={downloadAs}
-                        className='border-2 w-full h-10 p-2 mb-4 text-center'
-                    >
-                        <option>PNG</option>
-                        <option>JPEG</option>
-                    </select>
-                </div>
-                <div className=' flex justify-center '>
-                <button
-                 onClick={() => reset()}
-                 className='bg-purple-500 text-white font-semibold w-28 py-1 rounded shadow-lg mb-3'
-                 >Reset</button>
-                 </div>
-            </div>
-            <div className="bg-gray-100 h-full w-full p-2 lg:px-24 lg:py-16 rounded-lg">
-                <ComponentToImg downloadAs={downloadAs} >
-                    <BlogImage title={title} author={author} bgColor={bgColor} icon={icon}/>
-                </ComponentToImg>
-            </div>
+  return (
+    <div className="flex xl:flex-row flex-col text-black bg-gray-100 h-screen">
+      <div className="w-full xl:w-2/5 bg-white">
+        <h6 className="mt-8 ml-8 font-medium text-gray-500 mb-8">
+          💻 Creating cover images for your
+          <br />
+          <span className="ml-8">blogs is now super easy</span>
+        </h6>
+        <div className="border-2 border-gray-200 w-11/12 ml-auto mr-auto rounded mt-2 mb-1">
+          <input
+            type="text"
+            placeholder="Enter title here"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="w-full focus:outline-none focus:ring focus:border-blue-300
+                        leading-5 p-2 rounded text-center "
+          />
         </div>
-    )
-}
+        <div className="border-2 border-gray-200 w-11/12 ml-auto mr-auto rounded mt-1">
+          <input
+            type="text"
+            placeholder="Enter Author here"
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+            className="w-full focus:outline-none focus:ring focus:border-blue-300
+                        leading-5 p-2 rounded text-center "
+          />
+        </div>
+        <div className="w-11/12 border-2 mt-3 rounded text-center m-auto">
+          <h6 className="font-semibold text-gray-600 mb-2">Background</h6>
+          <div className="flex flex-row mb-2">
+            <div className="mr-auto ml-auto w-24 justify-center text-center ">
+              <span>
+                Color
+                <input
+                  type="color"
+                  value={bgColor}
+                  onChange={(e) => {
+                    setBgColor(e.target.value);
+                  }}
+                  className="w-24 h-10 pr-3 pl-3 p-1 bg-white border-2 flex-col rounded"
+                />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="text-center mt-2 w-4/5 m-auto">
+          <h6 className="font-semibold text-gray-600 mb-2">Font Style</h6>
+          <select
+            onChange={(e) => setFontStyle(e.target.value)}
+            value={fontStyle}
+            className="border-2 w-full h-10 p-2 mb-4 text-center"
+          >
+            <option>font-Oswald</option>
+            <option>font-serif</option>
+            <option>font-sans</option>
+            <option>font-mono</option>
+            <option>font-Inter</option>
+            <option>font-Poppins</option>
+            <option>font-Anek</option>
+          </select>
+        </div>
 
-export default BlogConfig
+        <div className="text-center mt-2 w-4/5 m-auto">
+          <h6 className="font-semibold text-gray-600 mb-2">Dev Icon</h6>
+          <Select
+            value={icon}
+            options={icons}
+            onChange={(e) => {
+              setIcon(e);
+            }}
+            className="uppercase"
+          />
+        </div>
+        <div className="text-center mt-2 w-4/5 m-auto">
+          <h6 className="font-semibold text-gray-600 mb-2">Download As</h6>
+          <select
+            onChange={(e) => setDownloadAs(e.target.value)}
+            value={downloadAs}
+            className="border-2 w-full h-10 p-2 mb-4 text-center"
+          >
+            <option>PNG</option>
+            <option>JPEG</option>
+          </select>
+        </div>
+        <div className=" flex justify-center ">
+          <button
+            onClick={() => reset()}
+            className="bg-purple-500 text-white font-semibold w-28 py-1 rounded shadow-lg mb-3"
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="bg-gray-100 h-full w-full p-2 lg:px-24 lg:py-16 rounded-lg">
+        <ComponentToImg downloadAs={downloadAs}>
+          <BlogImage
+            title={title}
+            author={author}
+            bgColor={bgColor}
+            icon={icon}
+            fontStyle={fontStyle}
+          />
+        </ComponentToImg>
+      </div>
+    </div>
+  );
+};
+
+export default BlogConfig;
--- src/Mainbody/BlogImage.jsx
+++ src/Mainbody/BlogImage.jsx
@@ -1,19 +1,23 @@
-const BlogImage = ({title, bgColor, author, icon}) => {
-    return (
-        <div style={{backgroundColor: bgColor}} 
-            className="flex flex-row w-full h-60 p-6 py-12 md:h-80 md:py-12 md:px-16 text-gray-600 ">
-                <div  className="w-full bg-white flex p-8 pl-2 md:p-12 md:pr-8 rounded-lg">
-                <i className={`devicon-${icon.value}-plain text-5xl m-2 mr-2 md:text-8xl md:m-4 md:mr-5 flex items-center`}></i>
-                    <div className="flex flex-col justify-center">
-                        <h1 className="text-lg md:text-4xl font-bold mb-4">{title}</h1>
-                        <hr className="border-2 bg-gray-800 w-1/2 mb-2" />
-                        <h3 className="md:text-3xl font-bold">{author}</h3>
-                    </div>
-                    
-                </div>
-            
+const BlogImage = ({ title, bgColor, author, icon, fontStyle }) => {
+  return (
+    <div
+      style={{ backgroundColor: bgColor }}
+      className="flex flex-row w-full h-60 p-6 py-12 md:h-80 md:py-12 md:px-16 text-gray-600 "
+    >
+      <div
+        className={`w-full bg-white flex p-8 pl-2 md:p-12 md:pr-8 rounded-lg ${fontStyle}`}
+      >
+        <i
+          className={`devicon-${icon.value}-plain text-5xl m-2 mr-2 md:text-8xl md:m-4 md:mr-5 flex items-center`}
+        ></i>
+        <div className="flex flex-col justify-center">
+          <h1 className={`text-lg md:text-4xl mb-4 `}>{title}</h1>
+          <hr className="border-2 bg-gray-800 w-1/2 mb-2" />
+          <h3 className={`md:text-3xl font-bold ${fontStyle}`}>{author}</h3>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BlogImage
+export default BlogImage;
